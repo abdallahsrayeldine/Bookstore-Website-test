e1b38209032e2cef449ab342f505a7c09bf9f511
Abdallah Srayeldine <122178319+abdallahsrayeldine@users.noreply.github.com>
2025-06-26 12:08:38 +0300

    Update connectDB.php

--- connectDB.php
+++ connectDB.php
@@ -1,2 +1,2 @@
 <?php
-$pdo = new PDO('mysql:host=localhost;port=3306;dbname=bookstore', 'root', 'password');
+$pdo = new PDO('mysql:host=bookstore-migration-bookstoredb-vczqyqs4oakr.ctouaoi6amkx.us-east-1.rds.amazonaws.com;port=3306;dbname=bookstore', 'root', 'password');
